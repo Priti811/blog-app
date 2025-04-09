512f6cdb22e1fc9c349b2a5746ed9c8203099af3
Priti Kumari <prittykumari64@gmail.com>
2025-04-10 00:11:16 +0530

    "change"

--- backend/server.js
+++ backend/server.js
@@ -15,6 +15,12 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/blogApp",
   useUnifiedTopology: true
 });
 
+
+app.use((req, res, next) => {
+  console.log(`Incoming request: ${req.method} ${req.url}`);
+  next();
+});
+
 // Routes
 app.post("/signup", async (req, res) => {
   const { username, password } = req.body;
