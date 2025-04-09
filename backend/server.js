8962f00896a9d7488517a6da67bbda8c7442d39a
Priti Kumari <prittykumari64@gmail.com>
2025-04-09 22:41:54 +0530

    "code changes"

--- backend/server.js
+++ backend/server.js
@@ -57,3 +57,12 @@ app.delete("/blogs/:id", async (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+
+
+// At the bottom of server.js
+const path = require('path');
+app.use(express.static(path.join(__dirname, '../client')));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/index.html'));
+});
+
